381a4387ff11d2f9208f847ae802f634a8f81b8d
badcommandorfilename <badcommandorfilename@hotmail.com>
2013-03-02 13:07:39 +1100

    Created: Tutorial 6 project with required assets

---
+++ Copperscript/Tutorial6/Tutorial6.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Html;
+using jQueryApi;
+
+namespace Tutorial6
+{
+    /// <summary>
+    /// Adaptation of Copperlicht Tutorial #6
+    /// http://www.ambiera.com/copperlicht/documentation/tutorials/tutorial-06.html
+    /// </summary>
+    public class Tutorial6
+    {
+    }
+}
