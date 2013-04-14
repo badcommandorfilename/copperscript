cfe88b8d7573717b48c0648ab81b0d6e948b36c5
badcommandorfilename <badcommandorfilename@hotmail.com>
2013-04-14 19:57:42 +1000

    Added access to low level WebGL objects

--- Copperscript/CopperLicht/Renderer.cs
+++ Copperscript/CopperLicht/Renderer.cs
@@ -9,16 +9,14 @@ namespace CL3D
     [ScriptNamespace("CL3D")]
     public class Renderer
     {
-        [ScriptName("setWorld")]
         public extern void setWorld(Matrix m);
-        [ScriptName("drawMesh")]
         public extern void drawMesh(Mesh m);
 
-        [ScriptName("getWidth")]
         public extern int getWidth();
-        [ScriptName("getHeight")]
         public extern int getHeight();
 
         public extern int createMaterialType(string vertex_shader_source, string fragment_shader_source);
+
+        public extern object getWebGL();
     }
 }
